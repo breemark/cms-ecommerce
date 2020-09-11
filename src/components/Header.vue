99181925514ec03fc4a5623b94fbc40a8c030552
breemark <britomaximiliano83@gmail.com>
2020-09-11 16:30:29 +0700

    router and navbar

---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <!-- <a class="navbar-brand" href="/">CMS Shopping Cart</a> -->
+    <router-link class="navbar-brand" to="/">CMS ShoppingCart</router-link>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarsExampleDefault"
+      aria-controls="navbarsExampleDefault"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active" v-for="(page, index) in pages" :key="index">
+          <router-link
+            v-if="page.slug != 'home'"
+            :to="`/${page.slug}`"
+            class="nav-link"
+          >{{page.name}}</router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  computed: {
+    ...mapGetters(["pages"]),
+  },
+};
+</script>
